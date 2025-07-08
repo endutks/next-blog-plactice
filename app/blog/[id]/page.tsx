94775430c1d75668de55ca15a2115bb7818496c4
endo <endo@bamv.co.jp>
2025-07-08 03:41:56 +0000

    シンプルブログ用に改編

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,13 +1,12 @@
 import {client} from '@/libs/microcms'
 import styles from './page.module.css'
-import dayjs from 'dayjs'
 
 type Props = {
     id: string
     title: string
-    body: string
+    content: string
     publishedAt: string
-    category: {name: string}
+    tags: {name: string}
 }
 
 async function getBlogPost(id: string): Promise<Props> {
@@ -21,14 +20,11 @@ export default async function BlogPostPage({params}: {params: Promise<{id: strin
     const {id} = await params //IDを取得
     const post = await getBlogPost(id)
 
-    const formattedDate = dayjs(post.publishedAt).format('YY.MM.DD')
-
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>{post.title}</h1>
-            <div className={styles.date}>{formattedDate}</div>
-            <div className={styles.category}>カテゴリー：{post.category && post.category.name}</div>
-            <div className={styles.post} dangerouslySetInnerHTML={{__html: post.body}} />
+            <div className={styles.tags}>カテゴリー：{post.tags && post.tags.name}</div>
+            <div className={styles.post} dangerouslySetInnerHTML={{__html: post.content}} />
         </main>
     )
 }
